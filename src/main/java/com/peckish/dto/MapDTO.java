26eca4063c242241717bd294f42053632421eee4
HongSaek-Ko <bh1152@naver.com>
2025-03-24 02:47:12 +0900

    영업일 - 요일 조건 추가

--- src/main/java/com/peckish/dto/MapDTO.java
+++ src/main/java/com/peckish/dto/MapDTO.java
@@ -7,8 +7,12 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.TextStyle;
+import java.util.Locale;
 import java.util.TimeZone;
 
 @Data
@@ -28,6 +32,7 @@ public class MapDTO {
     private boolean certificate;
     private LocalTime openTime;
     private LocalTime closeTime;
+    private String days;
     private String status;
     private String filename;
 
@@ -36,6 +41,8 @@ public class MapDTO {
         TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
         LocalTime currentTime = LocalTime.now();
+        DayOfWeek dayOfWeek = LocalDate.now().getDayOfWeek();
+        String currentDate = dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.KOREAN);
         MapDTO mapDTO = new MapDTO();
         mapDTO.setMapId(map.getMapId());
         mapDTO.setShopId(map.getShop().getShopId());
@@ -53,7 +60,8 @@ public class MapDTO {
             mapDTO.setOpen(map.getShop().getShopOwner().isOpen());
 //            mapDTO.setStatus(!mapDTO.isOpen() ? "closed" : (!(currentTime.isAfter(mapDTO.getOpenTime()) && currentTime.isBefore(mapDTO.getCloseTime())) ? "closed" : "opened"));
             // (시간 조건) 및 isOpen 둘 중 하나라도 open이면 opened
-            if((currentTime.isAfter(mapDTO.getOpenTime()) && currentTime.isBefore(mapDTO.getCloseTime())) || mapDTO.isOpen()) {
+            mapDTO.setDays(map.getShop().getShopOwner().getDays());
+            if((currentTime.isAfter(mapDTO.getOpenTime()) && currentTime.isBefore(mapDTO.getCloseTime())) || mapDTO.isOpen() && mapDTO.days.contains(currentDate)) {
                 mapDTO.setStatus("opened");
             } else {
                 mapDTO.setStatus("closed");
@@ -67,8 +75,9 @@ public class MapDTO {
             mapDTO.closeTime = LocalTime.parse(map.getShop().getShopUser().getCloseTime(), formatter);
             mapDTO.setFilename(map.getShop().getShopUser().getFilename());
             mapDTO.setOpen(map.getShop().getShopUser().isOpen());
+            mapDTO.setDays(map.getShop().getShopUser().getDays());
             // 제보된 가게의 경우 시간 조건만 맞으면 개점으로 판정.
-            mapDTO.setStatus(currentTime.isAfter(mapDTO.getOpenTime()) && currentTime.isBefore(mapDTO.getCloseTime()) ? "opened" : "closed");
+            mapDTO.setStatus(currentTime.isAfter(mapDTO.getOpenTime()) && currentTime.isBefore(mapDTO.getCloseTime()) && mapDTO.days.contains(currentDate) ? "opened" : "closed");
         }
         return mapDTO;
     }
